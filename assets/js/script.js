d8979f1c578bb0e9f93916944156b7dbdf4af4ab
RubyMary18 <rubymary18@gmail.com>
2024-02-29 18:39:37 +0530

    counter functionality

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+let num = document.querySelectorAll('.num');
+let list = document.querySelectorAll('.follower-container li');
+let speed = 200;
+
+num.forEach(number => {
+    let displayNum = 0;
+    let endNum = parseInt(number.getAttribute("data-value"));
+    let duration = endNum / speed;
+    
+    setInterval(function() {
+        displayNum = displayNum + duration;
+        
+        if(displayNum < endNum) {
+            number.textContent = Math.floor(displayNum);
+        }
+    },10)
+})
+    
